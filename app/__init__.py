578c7dc891ab0aa256d75d355ff2951d9dfe0143
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-05-18 17:44:15 -0400

    Login/Logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from .config import Configuration
-from .models import db   # New import
-from .routes import orders
+from .models import db, Employee   # New import
+from .routes import orders, session
+from flask_login import LoginManager, current_user
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)  # Configure the application with SQLAlchemy
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
